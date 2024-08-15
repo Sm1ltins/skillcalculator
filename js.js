6480da36e801c055f7bba696d6f26fdd41ca788e
Smilto <95125725+Sm1ltins@users.noreply.github.com>
2024-08-15 14:03:42 +0300

    version 0.2

--- js.js
+++ js.js
@@ -2,9 +2,21 @@ function calculateskill() {
     var kd = document.getElementById('killdeath').value;
     var wl = document.getElementById('winloss').value;
     var hs = document.getElementById('headshots').value;
-    var skill = +kd + +wl + +hs;
+    var wlreal = wl / 100;
+    var hsreal = hs / 100;
+    var skill = +kd + +wlreal + +hsreal;
     var skill1 = skill / 3;
     var roundedskill = Math.round(skill1 * 100) / 100;
     document.getElementById('result').textContent = "Skill: " + roundedskill;
 }
 
+function enforceMinMax(el) {
+    if (el.value != "") {
+      if (parseInt(el.value) < parseInt(el.min)) {
+        el.value = el.min;
+      }
+      if (parseInt(el.value) > parseInt(el.max)) {
+        el.value = el.max;
+      }
+    }
+  }
